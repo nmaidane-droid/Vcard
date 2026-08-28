a9ec476798cf88989f7a6376eb37663736d9da57
nmaidane-droid <nmaidane@hotmail.com>
2026-08-28 14:31:12 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker — Carte de visite numérique
    RÈGLE : incrémenter VERSION à chaque modification de index.html. */
-const VERSION = 'carte-v18';
+const VERSION = 'carte-v20';
 const SHELL = ['./', './?v=qr', './index.html', './manifest.json',
   './icon.svg', './icon.png', './logo-blanc.png'];
 
