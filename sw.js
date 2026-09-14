512d7d588701276779314674768e6a01617986d3
nmaidane-droid <nmaidane@hotmail.com>
2026-09-14 16:10:58 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker — Carte de visite numérique
    RÈGLE : reporter ici le numéro affiché par VERSION_APP dans index.html. */
-const VERSION = 'carte-1.5';
+const VERSION = 'carte-1.6';
 const SHELL = ['./', './?v=qr', './index.html', './manifest.json',
   './icon.svg', './icon.png', './logo-blanc.png'];
 
